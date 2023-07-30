4124c28ad1f85b39f66992f150d2493c235a1a45
asrytikov <asrytikov@gmail.com>
2023-07-30 21:08:16 +0400

    Project19_2

--- src/main/java/com/example/project19_2/controller/ProductController.java
+++ src/main/java/com/example/project19_2/controller/ProductController.java
@@ -1,2 +1,40 @@
-package com.example.project19_2.controller;public class ProductController {
+package com.example.project19_2.controller;
+
+import com.example.project19_2.model.Product;
+import com.example.project19_2.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String viewProducts(Model model){
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+        return "products.html";
+    }
+
+    @PostMapping("/products")
+    public String addProduct(
+            @RequestParam String name,
+            @RequestParam double price,
+            Model model
+    ){
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        productService.addProduct(product);
+
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+        return "products.html";
+    }
 }
--- src/main/java/com/example/project19_2/service/ProductService.java
+++ src/main/java/com/example/project19_2/service/ProductService.java
@@ -1,2 +1,21 @@
-package com.example.project19_2.service;public class ProductService {
+package com.example.project19_2.service;
+
+import com.example.project19_2.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public List<Product> findAll(){
+        return products;
+    }
 }
